689f1124830f87c2b05f93eb12f663cdc248fbdf
Shaikh Altamash <111693242+Altamashhhhhh@users.noreply.github.com>
2024-04-14 23:54:12 +0530

    updated

---
+++ index.js
@@ -0,0 +1,251 @@
+let shopCategory = [
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/f10917087a483040b557e4b18204312c.png?f=png?dim=256x0",
+    title: "Top Products",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/fa936f30b4563fc4abd187fb22fe5258.png?f=png?dim=256x0",
+    title: "Elderly Cares",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/1e622b0308ec3ab48887512eaa3488a5.png?f=png?dim=256x0",
+    title: "Personal Care",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/9cc9a28ea4513009966cae794114eefd.png?f=png?dim=256x0",
+    title: "Healthcare Devices",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/335dae76832d370c94f0440f5ba89e1f.png?f=png?dim=256x0",
+    title: "Health food and drinks",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/ce48d6d71ace3892945f0b0a64c1f0e9.png?f=png?dim=256x0",
+    title: "Skin Care ",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/11315fee93053980af48fd871bf687b8.png?f=png?dim=256x0",
+    title: "Mother and baby care",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/4405b7900b0b34fcb5c8572aae9d4b2e.png?f=png?dim=256x0",
+    title: "Accessories and Wearables",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/514d0d7d01a63502b4ebfec9ae26f4d2.png?f=png?dim=256x0",
+    title: "Fitness Supplements",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/ecad9a974e003fb987858b3ee81413c6.png?f=png?dim=256x0",
+    title: "Ayurvedic Care",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/af7a9b53ad1239659615c6895b0972d7.png?f=png?dim=256x0",
+    title: "Sexual Wellness",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/105ceca515ee30509f93309ae75306aa.png?f=png?dim=256x0",
+    title: "Diabetic Care",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/35d6d4e00e5d3058906cfa8b92ca799c.png?f=png?dim=256x0",
+    title: "Health Condition",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/dc96175686f135b5a22d1e57165d0246.png?f=png?dim=256x0",
+    title: "Home Care",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/ef15f03b682d37c994eb978d6c85c455.png?f=png?dim=256x0",
+    title: "Trending Products",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/discovery/categoryImages/2e58770425cc39a983d6d71fb0cc07e2.png?f=png?dim=256x0",
+    title: "Surrgical and Dressing",
+  },
+];
+
+let newLaunches = [
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+  {
+    image:
+      "https://cdn01.pharmeasy.in/dam/products_otc/Q84402/abzorb-total-skin-relief-dusting-powder-20-extra-bottle-of-120gm-2-1712152617.jpg?dim=1440x0",
+    title:
+      "Abzorb Total Skin Relief Dusting Powder (20% Extra) Bottle Of 120gm",
+    mrp: "MRP 599.00",
+    price: "347.32",
+    discount: "40%",
+  },
+
+
+];
+// let slideNum = 0;
+// const slides = document.querySelectorAll('.img-slide');
+// const totalSlides = slides.length;
+// const sliderWidth = slides[0].offsetWidth;
+
+// function autoSlider() {
+//     slideNum = (slideNum + 1) % totalSlides;
+//     const move = -slideNum * sliderWidth;
+//     document.querySelector('.img-sliders').style.transform = `translateX(${move}px)`;
+// }
+// setInterval(autoSlider, 2500);
+
+////// FOR IS FOR LAB TESTS SECTION ////////
+
+let sliderIndex = 0;
+
+function moveSlider(direction) {
+  const slides = document.querySelectorAll(".slider-item");
+  const totalSlides = slides.length;
+  const sliderWidth = slides[0].offsetWidth;
+  sliderIndex = (sliderIndex + direction + totalSlides) % totalSlides;
+  const movement = -sliderIndex * sliderWidth;
+  document.querySelector(
+    ".slider"
+  ).style.transform = `translateX(${movement}px)`;
+}
+
+                                       ////////BELOW JS FOR SHOP BY CATEGORY ////////
+
+let mainShopCategory = document.querySelector(".shopCategory");
+
+mainShopCategory.id = "mainShopCategory";
+mainShopCategory.className = "sliders";
+
+shopCategory.forEach((element) => {
+  let shopCategoryDiv = document.createElement("div");
+  shopCategoryDiv.id = "shopCategory";
+  shopCategoryDiv.className = "slider-items";
+
+  let img = document.createElement("img");
+  img.src = element.image;
+  let title = document.createElement("p");
+
+  title.innerText = element.title;
+
+  mainShopCategory.append(shopCategoryDiv);
+  shopCategoryDiv.append(img, title);
+});
+
+let sliderIndexs = 0;
+
+function moveSliders(direction) {
+  const slide = document.querySelectorAll(".slider-items");
+  const totalSlide = slide.length;
+  const slidersWidth = slide[0].offsetWidth;
+  sliderIndexs = (sliderIndexs + direction + totalSlide) % totalSlide;
+  const movements = -sliderIndexs * slidersWidth;
+  document.querySelector(
+    ".sliders"
+  ).style.transform = `translateX(${movements}px)`;
+}
+
+
+
+                                                       // BELOW CODE FOR NEW LAUNCHES Selection 
